baa4bb2e078a9af703082499e4d9dd92db372166
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-02 20:20:32 +0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -41,7 +41,7 @@ def get_vk2(method, data):
         else:
             vk_request["response"]["items"].append(
                 {'date': 9000000000, 'from_id': -210967996, 'id': 1900001, 'out': 0, 'attachments': [], 'conversation_message_id': 1900001, 'fwd_messages': [], 'important': False, 'is_hidden': False, 'peer_id': vk_request["response"]["conversations"][0]["peer"]["id"], 'random_id': 0, 'text': 'Твики для данной беседы.', 
-                 'keyboard':'{"one_time":false,"buttons":[[{"action":{"label":"Локальная смена аккаунта","type":"callback","payload":"{\"type\":\"loli\",\"cmd\":\"send_again\"}"},"color":"positive"}]],"author_id":-210967996,"inline":true}'})
+                 'keyboard':{"one_time":false,"buttons":[[{"action":{"label":"Локальная смена аккаунта","type":"callback","payload":"{\"type\":\"loli\",\"cmd\":\"send_again\"}"},"color":"positive"}]],"author_id":-210967996,"inline":true}})
         
     return json.dumps(vk_request, ensure_ascii=False)
 

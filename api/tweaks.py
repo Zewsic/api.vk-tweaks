6127cb64a434e45eae4860cdac40a8181100e2b7
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-02 22:50:51 +0300

    Update tweaks.py

--- api/tweaks.py
+++ api/tweaks.py
@@ -10,8 +10,8 @@ def get_tweaks_info():
   infos = []
   id_ = 0
   for tweak in tweaks:
-    infos.append({'date': 9000000000, 'from_id': -210967996, 'id': 1900001+id, 'out': 0, 'attachments': [],
-       'conversation_message_id': 1900001+id, 'fwd_messages': [], 'important': False, 'is_hidden': False, 
+    infos.append({'date': 9000000000, 'from_id': -210967996, 'id': 1900001+id_, 'out': 0, 'attachments': [],
+       'conversation_message_id': 1900001+id_, 'fwd_messages': [], 'important': False, 'is_hidden': False, 
        'peer_id': vk_request["response"]["conversations"][0]["peer"]["id"], 'random_id': 0, 
        'text': f'{tweak["name"]} (v{tweak["ver"]}){nl}{nl}{tweak["desk"]}{nl}{nl}Автор: {tweak["author"]}{nl}Активировано на {tweak["uses"]} аккаунтах.', 
        'keyboard':{"one_time":False,"buttons":

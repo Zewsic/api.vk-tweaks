3cf97a913239092bce71db3defd46f6ce6fbbb83
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-02 20:47:35 +0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -31,7 +31,7 @@ def get_vk2(method, data):
         data["access_token"] = usr_token
     if method == "messages.sendMessageEvent":
         if data['payload']['cmd'] == "test":
-            return requests.get(f"https://api.vk.com/method/messages.send?peer_id={vk_request["response"]["conversations"][0]["peer"]["id"]}&v=5.135&random_id=0&message=Даниил Питонов нажал на тестовую виртуальную кнопку&access_token={data["access_token"]}")
+            return requests.get(f'https://api.vk.com/method/messages.send?peer_id={vk_request["response"]["conversations"][0]["peer"]["id"]}&v=5.135&random_id=0&message=Даниил Питонов нажал на тестовую виртуальную кнопку&access_token={data["access_token"]}')
 
     if vk_request_url == "": vk_requests_url = get_vk_requests_url(method, data)
     print("API URL: " + vk_requests_url)

9a0608dc606be62314646d2fe429fcfbc4ed5d38
wex335 <wex335@yandex.ru>
2022-03-02 09:08:26 -0500

    fix update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from gunicorn.app.wsgiapp import run
 
 if __name__ == '__main__':
-    os.system('git pull origin master')
+    os.chdir("/root/vk-tweaks/api")
+    os.system('git pull')
     sys.argv = [sys.argv[0], '-w', '8', '-b', '0.0.0.0:3000', 'api:api','--access-logfile=-']
     sys.exit(run())

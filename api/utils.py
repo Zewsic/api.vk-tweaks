7ca27d4a6886e691ce3dc053671af54e554ae422
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-02 23:23:25 +0300

    Create utils.py

---
+++ api/utils.py
@@ -0,0 +1,16 @@
+def get_vk_requests_url(method, data):
+    new_data = []
+    data_keys = list(data.keys())
+    for key in data_keys:
+        new_data.append({"key":key, "value":data[key]})
+
+    vk_requests_url = f"https://api.vk.com/method/{method}?"
+
+    for param in new_data:
+        vk_requests_url += f"{param['key']}={param['value']}&"
+
+    vk_requests_url = vk_requests_url[0: -1]
+    return vk_requests_url
+
+def get_id_from_token(token):
+    return 1

b1eca035c343c37d64e7fed685424af3ce61d5a9
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-02 22:46:20 +0300

    Update tweaks.py

--- api/tweaks.py
+++ api/tweaks.py
@@ -4,5 +4,18 @@ tweaks = [
   {"name":"Local Chat Editer", "desk":"Позволяет локально редактировать информацию о диалоге.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"}
 ]
 
+nl = "\n"
+
 def get_tweaks_info():
-  pass
+  infos = []
+  id_ = 0
+  for tweak in tweaks:
+    infos.append({'date': 9000000000, 'from_id': -210967996, 'id': 1900001+id, 'out': 0, 'attachments': [],
+       'conversation_message_id': 1900001+id, 'fwd_messages': [], 'important': False, 'is_hidden': False, 
+       'peer_id': vk_request["response"]["conversations"][0]["peer"]["id"], 'random_id': 0, 
+       'text': f'{tweak["name"]} (v{tweak["ver"]}){nl}{nl}{tweak["desk"]}{nl}{nl}Автор: {tweak["author"]}{nl}Активировано на {tweak["uses"]} аккаунтах.', 
+       'keyboard':{"one_time":False,"buttons":
+                   [[{"action":{"label":"Активировать","type":"callback","payload":"cmd_test"},"color":"positive"}]]
+                   ,"author_id":-210967996,"inline":True}})
+  return infos
+        

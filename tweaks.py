de1f23c48440b4e82bee0ff52f25ff3190fb4b14
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-02 22:37:38 +0300

    Create tweaks.py

---
+++ tweaks.py
@@ -0,0 +1,8 @@
+tweaks = [
+  {"name":"GetUnblocked", "desk":"Позволяет вам просматривать страницы пользователей, если они вас заблокировали.", "author":"Zewsic", "uses":"0", "ver":"1.0 BETA"},
+  {"name":"Force UserID", "desk":"Принудительно использует UserID, даже если у пользователя имеется короткое имя.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"},
+  {"name":"Local Chat Editer", "desk":"Позволяет локально редактировать информацию о диалоге.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"}
+]
+
+def get_tweaks_info():
+  pass

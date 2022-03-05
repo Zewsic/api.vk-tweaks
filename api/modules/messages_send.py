273b14ef505ac23563a3ec315a834db650d0373a
wex335 <wex335@yandex.ru>
2022-03-05 14:11:06 -0500

    Done!

--- api/modules/messages_send.py
+++ api/modules/messages_send.py
@@ -7,7 +7,7 @@ def __call__(data,method):
         print(vm)
         doc = json.load(open('data/'+vm+'.json'))
         message = requests.get(f'https://api.vk.com/method/messages.send?v=5.135&access_token={data["access_token"]}').json()
-        return requests.get(f'https://api.vk.com/method/messages.send?peer_id={data["peer_id"]}&v=5.135&random_id=0&access_token={data["access_token"]}',{"attachment":f'doc{doc["owner_id"]}_{doc["id"]}'}).json()
+        return requests.get(f'https://api.vk.com/method/messages.send?peer_id={data["peer_id"]}&v=5.135&random_id=0&access_token={data["access_token"]}&reply_to={data.get("forward_messages",data.get("reply_to"))}',{"attachment":f'doc{doc["owner_id"]}_{doc["id"]}'}).json()
 
             
     elif (text.startswith('!гс')):
@@ -24,7 +24,7 @@ def __call__(data,method):
             files = [('file', (vm+'.ogg', open('data/'+vm+'.ogg', 'rb')))]
             file = requests.post(serv['response']['upload_url'] , files=files).json()['file']
             result = requests.get(f'https://api.vk.com/method/docs.save?v=5.135&access_token={data["access_token"]}',{'file': file }).json()
-            with open('data/'+vm+'.json',"w") as dd:
+            with open('data/'+""+vm+'.json',"w") as dd:
                 json.dump(result['response']['audio_message'],dd)
             return {'response': 1900002}
     else:

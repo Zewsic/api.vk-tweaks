4d51d01edd04d7ea831784444860c84256659b9a
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-05 19:08:09 +0300

    Update tweaks.py

--- api/tweaks.py
+++ api/tweaks.py
@@ -1,7 +1,10 @@
+from tweaks import *
+from utils import *
+
 tweaks = [
-  {"name":"GetUnblocked", "desk":"Позволяет вам просматривать страницы пользователей, если они вас заблокировали.", "author":"Zewsic", "uses":"0", "ver":"1.0 BETA"},
-  {"name":"Force UserID", "desk":"Принудительно использует UserID, даже если у пользователя имеется короткое имя.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"},
-  {"name":"Local Chat Editer", "desk":"Позволяет локально редактировать информацию о диалоге.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"}
+  {"name":"GetUnblocked", "desk":"Позволяет вам просматривать страницы пользователей, если они вас заблокировали.", "author":"Zewsic", "uses":"0", "ver":"1.0 BETA"}
+  #{"name":"Force UserID", "desk":"Принудительно использует UserID, даже если у пользователя имеется короткое имя.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"},
+  #{"name":"Local Chat Editer", "desk":"Позволяет локально редактировать информацию о диалоге.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"}
 ]
 
 nl = "\n"
@@ -15,7 +18,7 @@ def get_tweaks_info():
            'peer_id': -210967996, 'random_id': 0, 
            'text': f'{tweak["name"]} {tweak["ver"]}{nl}{nl}{tweak["desk"]}{nl}{nl}Автор: {tweak["author"]}{nl}Активировано на {tweak["uses"]} аккаунтах.', 
            'keyboard':{"one_time":False,"buttons":
-                       [[{"action":{"label":"Активировать","type":"callback","payload":"cmd_test"},"color":"positive"}]]
+                       [[{"action":{"label":"Активировать","type":"callback","payload":f"toggle_tweak {id_}"},"color":"positive"}]]
                        ,"author_id":-210967996,"inline":True}})
         id_ += 1
     return infos
@@ -23,10 +26,9 @@ def get_tweaks_info():
 def parse_preRequest(method, data):
   if method == "execute.getFullProfileNewNew": 
     if tweak_check(get_id_from_token(token), 0):
-      data["access_token"] = usr_token
+      data = getUnblocked.execute(data)
   if method == "messages.sendMessageEvent":
-    if data['payload'] == "cmd_test":
-      requests.get(f'https://api.vk.com/method/messages.send?peer_id={data["peer_id"]}&v=5.135&random_id=0&message=VK Tweaks: Тестовая кнопка нажата&access_token={data["access_token"]}').text
+    requests.get(f'https://api.vk.com/method/messages.send?peer_id={data["peer_id"]}&v=5.135&random_id=0&message=VK Tweaks callback:{data["payload"]}&access_token={data["access_token"]}').text
   return data
     
 

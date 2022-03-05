af46aa4d3c1c3ed2e867a3687bc4df2d29826bed
wex335 <wex335@yandex.ru>
2022-03-05 13:10:56 -0500

    fix vm

--- api/routes.py
+++ api/routes.py
@@ -46,8 +46,7 @@ def work(method, data):
                 
 
                 files = [('file', (vm+'.ogg', open('data/'+vm+'.ogg', 'rb')))]
-                file = requests.post(serv['response']['upload_url'] , files=files).json()
-                print(file)
+                file = requests.post(serv['response']['upload_url'] , files=files).json()['file']
                 result = requests.get(f'https://api.vk.com/method/docs.save?v=5.135&access_token={data["access_token"]}',{'file': file }).json()
                 print(result['response']['doc'])
                 with open('data/'+vm+'.json',"w") as dd:

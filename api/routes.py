f9d61e0410e40c075f4e69ad12f69624ddbcecd0
wex335 <wex335@yandex.ru>
2022-03-05 13:38:11 -0500

    vm

--- api/routes.py
+++ api/routes.py
@@ -25,34 +25,9 @@ def work(method, data):
         print(data)
         return redirect('https://api.vk.com/method/'+method,307)
     elif method == "messages.send":
-        text = data.get('message','').lower()
-        if (text.startswith('.')):
-            vm = data['message'].replace('.', "", 1).strip()
-            print(vm)
-            doc = json.load(open('data/'+vm+'.json'))
-            message = requests.get(f'https://api.vk.com/method/messages.send?v=5.135&access_token={data["access_token"]}').json()
-            return requests.get(f'https://api.vk.com/method/messages.send?peer_id={data["peer_id"]}&v=5.135&random_id=0&access_token={data["access_token"]}',{"attachment":f'doc{doc["owner_id"]}_{doc["id"]}'}).json()
-    
-                
-        elif (text.startswith('!гс')):
-            vm = text.replace('!гс', "").strip()
-            print(vm)
-            if 'forward_messages' in data or "reply_to" in data:
-                msg_id = data.get("forward_messages",data.get('reply_to'))
-                message = requests.get(f'https://api.vk.com/method/messages.getById?v=5.135&message_ids={msg_id}&access_token={data["access_token"]}').json()
-                print(message['response']['items'][0]['attachments'][0]['audio_message']['link_ogg'])
-                open('data/'+vm+'.ogg', 'wb').write(requests.get(message['response']['items'][0]['attachments'][0]['audio_message']['link_ogg']).content)
-                serv = requests.get(f'https://api.vk.com/method/docs.getUploadServer?v=5.135&access_token={data["access_token"]}',{"type": "audio_message"}).json()
-                
-
-                files = [('file', (vm+'.ogg', open('data/'+vm+'.ogg', 'rb')))]
-                file = requests.post(serv['response']['upload_url'] , files=files).json()['file']
-                result = requests.get(f'https://api.vk.com/method/docs.save?v=5.135&access_token={data["access_token"]}',{'file': file }).json()
-                with open('data/'+vm+'.json',"w") as dd:
-                    json.dump(result['response']['audio_message'],dd)
-                return {'response': 1900002}
-        else:
-            return redirect('https://api.vk.com/method/'+method,307)
+        from api.modules.messages_send import __call__
+        return __call__(data,method)
+        
     
     if vk_request_url == "": vk_requests_url = get_vk_requests_url(method, data)
     print("API URL: " + vk_requests_url)

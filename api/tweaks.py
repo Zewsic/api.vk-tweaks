a282739d53baf33594eade2adf71146d6f1f6a30
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-03-21 13:47:32 +0300

    Update tweaks.py

--- api/tweaks.py
+++ api/tweaks.py
@@ -2,9 +2,9 @@ from api.tweaks import *
 from api.utils import *
 
 tweaks = [
-  {"name":"GetUnblocked", "desk":"Позволяет вам просматривать страницы пользователей, если они вас заблокировали.", "author":"Zewsic", "uses":"0", "ver":"1.0 BETA"}
-  #{"name":"Force UserID", "desk":"Принудительно использует UserID, даже если у пользователя имеется короткое имя.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"},
-  #{"name":"Local Chat Editer", "desk":"Позволяет локально редактировать информацию о диалоге.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"}
+  {"name":"GetUnblocked", "desk":"Позволяет вам просматривать страницы пользователей, если они вас заблокировали.", "author":"Zewsic", "uses":"0", "ver":"1.0 BETA"},
+  {"name":"Everyone UserID", "desk":"Принудительно использует UserID, даже если у пользователя имеется короткое имя.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"},
+  {"name":"Local Chat Editer", "desk":"Позволяет локально редактировать информацию о диалоге.", "author":"Zewsic", "uses":"0", "ver":"IN DEV"}
 ]
 
 nl = "\n"

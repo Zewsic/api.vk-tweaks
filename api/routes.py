f0cfe853436191f8b6ceb57364bd2a39c68502e5
Zewsic <66947765+Zewsic@users.noreply.github.com>
2022-04-05 19:32:53 +0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -16,22 +16,14 @@ def work(method, data):
     
     
     data = parse_preRequest(method, data, token)
+    
     if vk_request_url == "": vk_requests_url = get_vk_requests_url(method, data)
     print("API URL: " + vk_requests_url)
     vk_request = requests.get(vk_requests_url).json()
     
-    #POST-REQUEST TWEAKS
-    if method == "messages.getHistory": #VK Tweaks Menu
-        if vk_request["response"]["conversations"][0]["peer"]["id"] == -210967996:
-            vk_request["response"]["items"] = get_tweaks_info(token)
-        else:
-            if tweak_check(get_id_from_token(token), 2): #Chat Editor
-                vk_request["response"]["items"].append(
-                    {'date': 9000000000, 'from_id': -210967996, 'id': 1900001, 'out': 0, 'attachments': [], 'conversation_message_id': 1900001, 'fwd_messages': [], 'important': False, 'is_hidden': False, 'peer_id': vk_request["response"]["conversations"][0]["peer"]["id"], 'random_id': 0, 'text': 'Твики для данной беседы.', 
-                    'keyboard':{"one_time":False,"buttons":[[{"action":{"label":"Тестовая кнопка","type":"callback","payload":"cmd_test"},"color":"positive"}]],"author_id":-210967996,"inline":True}})
-            if tweak_check(get_id_from_token(token), 3): #Neirobot
-                vk_request["response"]["items"][0]["keyboard"] = {"one_time":True,"buttons":[[{"action":{"label":f"Ответ нейробота {str(i+1)}","type":"text"},"color":"secondary"}] for i in range(3)],"author_id":vk_request["response"]["items"][0]['from_id'], "inline":False}
-                
+    vk_request = parse_postRequest(method, vk_request, token)
+    
+        
     return json.dumps(vk_request, ensure_ascii=False)
 
 need = ['execute.getFullProfileNewNew',
